b01ae7ea40f2cf4d79aeca336e28fbf8b7451951
koyae <koyae@users.noreply.github.com>
2017-01-30 17:48:05 -0700

    Updated komodo keybindings and macros. Fixed incomplete setup-script. Updated GIMP-accelerators.

--- bin2/setup.sh
+++ bin2/setup.sh
@@ -2,28 +2,31 @@
 
 if [[ -f ~/.gitconfig1 ]]; then
 	read -r -d '' def <<-'EOF'
-		:function Verytemporary()
+		:function! Verytemporary()
 			let gohere = search('^\[include\]')
 			if gohere==0
+			" If nothing found:
 				" Go to top:
 				normal! gg
-				" Create [include] section:
-				normal! i[include]
+				" Create [include] section and bump whatever was on first line:
+				execute "normal! i[include]\<Return>\<Esc>"
 			endif
-
 			let gohere = search('path \?= \? .gitconfig1') 
-			if gohere==0 
-				" Insert a newline:
-				normal! o
-				" Write line that does import:
-				normal! ipath = .gitconfig1
+			if gohere==0
+				" Jump to appropriate line:
+				execute gohere
+				" Write a line below that does import:
+				normal! opath = .gitconfig1
 				" Do indent:
 				normal! V>
+				" Save changes:
 				write
+				" Exit vim:
+				quit
 			endif
 		endfunction
 	EOF
-	vim ~/.gitconfig -c "$def"
+	vim ~/.gitconfig -c "$def" -c ":call Verytemporary()"
 else
 	echo "Gitconfig supplement at $target not found."
 	kill -INT $$
