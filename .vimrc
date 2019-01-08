koyae/migration

commit 18d664246f9fa20c7fe4dbc0861d7632ab9236a3

Rewrote append-to-file function in vimrc to fix it on Ubuntu, as the shellescape() function was adding unwanted backslashes to the output text.

.vimrc
@@ -768,13 +768,15 @@ autocmd BufNewFile,BufRead, *.postgre.sql setf pgsql
 		let suffdict.sql = suffdict.pgsql
 		let text = a:0 >= 1 ? a:1 : GetSelectionText()
 		let fifoPath = a:0 >= 2 ? a:2 : '/tmp/fif'
-		let text = printf(
-			\ 'printf %s %s >> %s',
-			\ shellescape('%s\n'),
-			\ shellescape(text . get(suffdict,&syntax,'')),
+		" Below, first backslash prevents vim from expandeding '%' to current
+		" filename and the second backslash allows the '\n' to actually reach
+		" `printf`:
+		silent execute
+			\ "!printf"
+			\ '\%s\\n'
+			\ shellescape(text . get(suffdict,&syntax,''))
+			\ . ' >>'
 			\ shellescape(fifoPath)
-		\ )
-		call system(text)
 	endfunction
 
 	" PipeToSocket(text[,socketPath])

commit 9120f6acce498d6758cee0a03297f3bdcb4837eb

Fixed append-to-file function in vimrc again because 18d6642 broke things for Cygwin. New approach should have good compatibility and appears to run faster than the old implementation to boot.

.vimrc
@@ -775,12 +775,12 @@ autocmd BufNewFile,BufRead, *.postgre.sql setf pgsql
 		" Below, first backslash prevents vim from expandeding '%' to current
 		" filename and the second backslash allows the '\n' to actually reach
 		" `printf`:
-		silent execute
-			\ "!printf"
-			\ '\%s\\n'
-			\ shellescape(text . get(suffdict,&syntax,''))
-			\ . ' >>'
-			\ shellescape(fifoPath)
+		" echom
+		call writefile(
+			\ split( text . get(suffdict,&syntax,''), "\n", 1 ),
+			\ fifoPath
+		\ )
+		" ^ vim.1045645.n5.nabble.com/Write-register-contents-to-file-tp5610081p5610229.html
 	endfunction
 
 	" PipeToSocket(text[,socketPath])