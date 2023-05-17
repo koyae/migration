6b6018c3fd05d781fff7222a243ab81546af6c46
Patrick O'Toole <koyae@users.noreply.github.com>
2023-05-16 19:09:51 -0600

    Added setting to gitconfig and improved git setup script.

--- bin2/setup.sh
+++ bin2/setup.sh
@@ -3,40 +3,23 @@
 oldcwd=`command pwd`
 cd ~
 git config user.email "koyae@users.noreply.github.com"
-cd "$oldcwd"
-
-if [ -f ~/.gitconfig1 ]; then
-	f="$(mktemp)"
-	read -r -d '' stuff <<-'EOF'
-		:function! Verytemporary()
-			let gohere = search('^\[include\]')
-			if gohere==0
-			" If nothing found:
-				" Go to top:
-				normal! gg
-				" Create [include] section and bump whatever was on first line:
-				execute "normal! i[include]\<Return>\<Esc>"
-			endif
-			let gohere = search('path \?= \? .gitconfig1')
-			if gohere==0
-				" Jump to appropriate line:
-				execute gohere
-				" Write a line below that does import:
-				normal! opath = .gitconfig1
-				" Do indent:
-				normal! V>
-				" Save changes:
-				write
-				" Exit vim:
-				quit
-			endif
-		endfunction
-		:call Verytemporary()
-	EOF
-	printf '%s' "$stuff"
-	vim ~/.gitconfig -c "so $f"
-	rm -f "$f"
-else
-	echo "Gitconfig supplement at $target not found."
-	kill -INT $$
+nothing_to_overwrite=0
+include_path="$(git config --global include.path)"
+if [ $? -eq 1 ]; then
+	nothing_to_overwrite=1
 fi
+printf '%s' "$include_path" | grep -qe '\.gitconfig1'
+if [ $? -ne 0 ]; then
+	if [ $nothing_to_overwrite -ne 1 ]; then
+		echo "Previous value(s) of git include.path present:" 1>&2
+		git config --global --get-all include.path | sed 's/^/\t/' 1>&2
+		printf '%s%s\n' "... Nondestructively added include path to"\
+		' $GIT_CONFIG_GLOBAL, ~/.gitconfig, or $XDG_CONFIG_PATH/git/config'\
+		"(whichever was found first)"
+	fi
+	git config --global --add include.path "$(pwd -P)/.gitconfig1"
+else
+	echo "Git include.path appears to be correct: $include_path" 1>&2
+	echo "(Doing nothing.)" 1>&2
+fi
+cd "$oldcwd"
